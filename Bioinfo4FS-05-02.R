a389c24f1c1806a3af51056aac9511ee502fe41d
mendozapelusa <mendozapelusa@users.noreply.github.com>
2019-05-06 15:02:29 +0200

    Multiple plots in one figure!
    The two plots that we get from running lines 26-46 are the ones I uploaded under the name of "Methylation_Density_XXX", for both cancer and control groups.

--- Bioinfo4FS-05-02.R
+++ Bioinfo4FS-05-02.R
@@ -16,12 +16,31 @@ hist(log_mean_coverage, breaks = 500, xlim = c(0,6))
 abline(v = summary(log_mean_coverage)[2:5], col = c("blue", "red", "black", "orange"), lty = 2)
 # This histogram shows the distribution of coverage values. 
 
-# Let's try now to get the density function of methylation in one sample, for example the first one (AML_myel_BM_S004XMA1_ctr.bed)
+# Let's try now to get the density function of methylation in one sample, 
+# for example the first one (AML_myel_BM_S004XMA1_ctr.bed)
 sample_01_beta <- genes_beta$AML_myel_BM_S004XMA1_ctr.bed 
 sample_01_beta_density <- density(sample_01_beta, na.rm = TRUE) #Important: Removal of missing values 
 plot(sample_01_beta_density)
 # Histogram to compare with actual counts instead of relative density
 hist(sample_01_beta, breaks = 100)
 
-#Next we would plot all density functions of a group (e.g. cancer patients) together with the par command? 
-#Maybe do some sort of comparison between groups?
+#Next we would plot all density functions of a group (e.g. cancer patients) together with the par command
+# For cancer patients
+par(mfrow=c(3,3))
+samplename <- colnames(genes_beta)
+for(i in 1:9){
+  beta_dens <- density(genes_beta[,i], na.rm=T)
+  plot(beta_dens, main = samplename[i], ylim=c(0,8))
+}
+
+# For the healthy patients
+par(mfrow=c(3,3))
+samplename <- colnames(genes_beta)
+for(i in 10:18){
+  beta_dens <- density(genes_beta[,i], na.rm=T)
+  plot(beta_dens, main = samplename[i], ylim=c(0,8))
+}
+
+#Roughly, we can observe a higher density of methylation in cancer samples (mind the scale)
+#How could we make a more quantitative comparison now? 
+
