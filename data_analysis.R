6c92fd0695f775a6b93c0bd161a2c1a3c81aebff
MatteoSpa <49938420+MatteoSpa@users.noreply.github.com>
2019-05-22 16:46:11 +0200

    Update data_analysis.R

--- data_analysis.R
+++ data_analysis.R
@@ -2,3 +2,17 @@ input_data <- readRDS(file="/Users/krank/Documents/Uni/4. Fachsemester/Bioinform
 install.packages("rmarkdown")
 sample_annotation <- read.csv(file="/Users/krank/Documents/Uni/4. Fachsemester/Bioinformatik/AMLvsGran/sample_annotation.csv")
 
+#Split data in order to facilitate data cleanup
+
+Genes <- input_data$genes;
+Promoters <- input_data$promoters;
+CpGislands <- input_data$cpgislands;
+
+Patient_Info <- sample_annotation[, which(colnames(sample_annotation) %in% c("sampleName", "cellTypeShort", "cellTypeGroup", "tissueTypeShort", "EXPERIMENT_ID", "DONOR_AGE", "DONOR_HEALTH_STATUS", "DONOR_SEX", "DONOR_ETHNICITY", "DONOR_REGION_OF_RESIDENCE", "GENETIC_CHARACTERISTICS", "TREATMENT"))]
+row.names(Patient_Info) = Patient_Info$sampleName
+Patient_Info = Patient_Info[, -which(colnames(Patient_Info) %in% c("sampleName"))]
+Healthy_Patients <- Patient_Info[which(Patient_Info$DONOR_HEALTH_STATUS == Healthy),]
+
+GenesOnly <- Genes[,-c(1,2,3,4,5,6,7,8,9,10,11)]
+BValues <- GenesOnly[,c(1:18)]
+Coverage<- GenesOnly[,c(19:35)]
