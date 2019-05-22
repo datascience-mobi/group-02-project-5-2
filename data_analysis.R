510b2eb02c8eb7592a116d5042cbb7e0673cc40b
kiki176 <k.krank@stud.uni-heidelberg.de>
2019-05-22 16:30:32 +0200

    The quality control code

---
+++ data_analysis.R
@@ -0,0 +1,4 @@
+input_data <- readRDS(file="/Users/krank/Documents/Uni/4. Fachsemester/Bioinformatik/AMLvsGran/AML_gran_list.RDS.gz")
+install.packages("rmarkdown")
+sample_annotation <- read.csv(file="/Users/krank/Documents/Uni/4. Fachsemester/Bioinformatik/AMLvsGran/sample_annotation.csv")
+
