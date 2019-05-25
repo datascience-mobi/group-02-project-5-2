db24535545dcc9aae3f219c0470a451c8894e29f
MatteoSpa <49938420+MatteoSpa@users.noreply.github.com>
2019-05-25 15:22:23 +0200

    Update treshold_code.R

--- treshold_code.R
+++ treshold_code.R
@@ -6,17 +6,17 @@ sample_annotation <- read.csv(file="/Users/krank/Documents/Uni/4. Fachsemester/B
 Genes <- input_data$genes
 Coverage<- GenesOnly[,c(19:35)]
 
-# Plotten der Häufigkeiten bestimmter Coverage-Werte
+# Plotten der HÃ¤ufigkeiten bestimmter Coverage-Werte
 Coverage_hist <- unlist(Coverage)
 log_cover <- log10(Coverage_hist)
 hist(log_cover, breaks = 200)
 
-# schleife zum Herausfinden, wie viele Werte wir je nach Treshold verlieren und dem plotten 
+# schleife zum Herausfinden, wie viele Werte wir je nach Threshold verlieren und dem plotten 
 
 tot_Unreliable <- c(1:50)
 for(i in 1:50){
-  treshold <- 1000000-i*5000
-  Unreliable <- Coverage_hist[which(Coverage_hist > treshold)]
+  threshold <- 1000000-i*5000
+  Unreliable <- Coverage_hist[which(Coverage_hist > threshold)]
   N_Unreliable <- length(Unreliable)
   tot_Unreliable[i] <- N_Unreliable
   }
@@ -24,6 +24,6 @@ for(i in 1:50){
 tot_Unreliable  
 
 x_axis <- seq(750000, 995000, by = 5000)
-plot( x=x_axis, y=tot_Unreliable, type= "l", main = "Unreliable Data by treshold", xlab= "Treshold position", ylab = "Unreliable Data")
+plot( x=x_axis, y=tot_Unreliable, type= "l", main = "Unreliable Data by threshold", xlab= "Threshold position", ylab = "Unreliable Data")
 abline(v=880000, col="red") # Our upper treshold could very well be 880.000 !
 
